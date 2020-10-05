ad38419722e7d3bf1620fd5ac71a3d0dd07ad52f
mochi-home <mochi.mls.ntl@gmail.com>
2020-10-06 00:57:06 +0900

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,4 +83,36 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
+  test "feed should have the right post" do
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+
+    # check following user's posts
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+
+    # check my posts
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+
+    # check that not followed users post is not exist
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
